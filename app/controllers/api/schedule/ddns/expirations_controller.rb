73ae40b9d3e8ba07642739683ae2573f0e56533b
Ryan <ryan@LiYu-KuangdeMacBook-Pro.local>
2016-09-23 17:17:27 +0800

    update event descriptions of ddns expiration

--- app/controllers/api/schedule/ddns/expirations_controller.rb
+++ app/controllers/api/schedule/ddns/expirations_controller.rb
@@ -3,7 +3,7 @@ class Api::Schedule::Ddns::ExpirationsController < Api::Base
 	def index
 		log_array = Services::DdnsExpire.notice
     info = {
-      :event => "[DDNS_CRON] notice by email",
+      :event => "[DDNS_EXPIRATION_CRON] Notice users by email",
       :count => log_array.size,
       :result => log_array
     }.to_json
@@ -14,7 +14,7 @@ class Api::Schedule::Ddns::ExpirationsController < Api::Base
 	def destroy
     log_array = Services::DdnsExpire.delete
     info = {
-      :event => "[DDNS_CRON] delete ddns and route53",
+      :event => "[DDNS_EXPIRATION_CRON] Delete DDNS and Route53 records",
       :count => log_array.size,
       :result => log_array
     }.to_json
