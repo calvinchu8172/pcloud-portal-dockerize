2fe0b3ac3589a50031eebbd04a625fd0bbfa21a1
Calvin Chu <calvinchu@ecoworkinc.com>
2016-10-24 11:36:41 +0800

    [#21767] Personal Cloud Portal insecure assets issue
    * Override Paperclip method to enable AWS Cloudfront Behavior Query String Whitelist setting.
    
    ref #21767

--- config/initializers/override.rb
+++ config/initializers/override.rb
@@ -1,7 +1,7 @@
 module Paperclip
   class UrlGenerator
     private
-
+    # Override Paperclip method to enable AWS Cloudfront Behavior Query String Whitelist setting.
     def timestamp_as_needed(url, options)
       if options[:timestamp] && timestamp_possible?
         delimiter_char = url.match(/\?.+=/) ? '&' : '?'
